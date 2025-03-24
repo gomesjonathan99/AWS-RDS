c7cd5b92bbaa44fd5491413ce5db89e0baa5d6f4
joshuadmello04950 <crce.9886.ce@gmail.com>
2025-03-24 17:03:05 +0530

    first commit

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 
-DB_USER = os.getenv("DB_USER", "admin")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "Mphasis123")
-DB_HOST = os.getenv("DB_HOST", "database-1.cdiwumagq0uz.us-east-1.rds.amazonaws.com")
-DB_NAME = os.getenv("DB_NAME", "database-1")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "joshua4christ7")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_HOST = os.getenv("DB_HOST", "database-1.cxs64eo44osy.ap-south-1.rds.amazonaws.com")
+DB_NAME = os.getenv("DB_NAME", "college_registration")
 
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
